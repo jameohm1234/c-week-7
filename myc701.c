43c3a45c9eda30d4e6537d15929a08defd5bb5cb
jameohm1234 <69183197+jameohm1234@users.noreply.github.com>
2020-08-25 11:59:47 +0700

    Add files via upload

---
+++ myc701.c
@@ -0,0 +1,17 @@
+/*
+Program : datatype
+By : jame saksit kumkeaw
+Date: 8/25/63
+*/
+#include<stdio.h>
+void main(){
+    short a;
+    long b;
+    long c;
+    long double d;
+    printf("size of short =%d byte\n", sizeof(a));
+    printf("size of short =%d byte\n", sizeof(b));
+    printf("size of long long =%d byte\n", sizeof(c));
+    printf("size of long double =%d byte\n", sizeof(d));
+    return 0 ;
+}
